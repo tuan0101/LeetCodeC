c4a9bc297b2cf4e961be7b0f1333fc8b2b85a5ae
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-08 16:17:53 -0800

    Time: 0 ms (100.00%), Space: 14.8 MB (48.54%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,22 +11,40 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL) return list2;
-        if(list2==NULL) return list1;
-        if(list1->val < list2->val){
-            list1->next = mergeTwoLists(list1->next, list2);
-            return list1;
+        ListNode* dummyHead = new ListNode(); //ListNode(-1) where -1 is any value
+        ListNode* tail = dummyHead;
+        
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                tail->next = list1;
+                list1 = list1->next;
+            }else{
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
         }
-        else{
-            list2->next = mergeTwoLists(list2->next, list1);
-            return list2;
-        }
+        
+        if(list1) tail->next = list1;
+        else if(list2) tail->next = list2;
+        return dummyHead->next;
+        
+        // recursive
+        // Time: O(m+n) Space:O(m+n) because of the call stack
+        // if(list1==NULL) return list2;
+        // if(list2==NULL) return list1;
+        // if(list1->val < list2->val){
+        //     list1->next = mergeTwoLists(list1->next, list2);
+        //     return list1;
+        // }
+        // else{
+        //     list2->next = mergeTwoLists(list2->next, list1);
+        //     return list2;
+        // }
     }
 };
 
-// if list1 is empty => return list2
-// if list1 < list2
-
-// 1,4,5
-// 1,2,6
-// 1,4,5
+// recursive call stack: (from bottom to top)
+// list1->next 
+// list1->next =mergeTwoLists(l1,l2)
+// list2->next = mergeTwoLists(l1,l2)
