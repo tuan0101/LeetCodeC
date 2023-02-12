e677f567fdde4282bcec9852f5ad713560062b99
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-12 13:19:23 -0800

    Time: 4 ms (95.42%), Space: 7.4 MB (62.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        // 6ms 7.2mb
         if (s.length() != t.length()) return false;
         int n = s.length();
         int counts[26] = {0};
+        
         for (int i = 0; i < n; i++) { 
             counts[s[i] - 'a']++;
             counts[t[i] - 'a']--;
         }
+        
         for (int i = 0; i < 26; i++)
             if (counts[i]) return false;
+        
         return true;
+        
         // 4ms 7.4mb
 //         int letters[26] = {0};
 //         int len = s.length();
