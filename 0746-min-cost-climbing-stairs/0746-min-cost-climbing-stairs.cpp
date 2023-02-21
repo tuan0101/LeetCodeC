fe8dc8a1561ace7c6785e3bde38f437dea5d2e37
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-20 20:27:21 -0800

    Time: 3 ms (95.26%), Space: 13.7 MB (58.08%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -17,13 +17,24 @@ public:
         
 //         return minCost;
         
-        cost.push_back(0);
+        // IF THE INPUT IS NOT READ-ONLY
+        // 7ms 13.7mb
+        // backward
+//         cost.push_back(0);
+//         int len = cost.size();
+//         for(int i=len-3; i>=0; i--){
+//             cost[i] += min(cost[i+1], cost[i+2]);
+//         }
+        
+//         return min(cost[0], cost[1]);
+        
+        // forward
         int len = cost.size();
-        for(int i=len-3; i>=0; i--){
-            cost[i] += min(cost[i+1], cost[i+2]);
+        for(int i=2; i<len; i++){
+            cost[i] += min(cost[i-1], cost[i-2]);
         }
         
-        return min(cost[0], cost[1]);
+        return min(cost[len-1], cost[len-2]);
     }
 };
 
