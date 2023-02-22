c9867ff89c87fab272cbb6c84e81e9fb330f206d
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-21 17:55:08 -0800

    Time: 7 ms (77.73%), Space: 7.6 MB (96.70%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -21,15 +21,31 @@ public:
 //         return dp[len];
         
         // 100% 0ms | 91% 7.7mb
+        int len = s.size();
+        vector<bool> dp(len+1,0);
+
+        dp[0] = 1;
+        for(int i=1; i<=len ;i++)
+            for(string &w : wordDict){
+                int wordSize = w.size();
+                if(i>=wordSize && dp[i-wordSize] 
+                   && s.substr(i-wordSize, wordSize)==w)
+                {
+                    dp[i] = 1;
+                    break;
+                }
+            }
+
+        return dp[len];
+        
 //         int len = s.size();
 //         vector<bool> dp(len+1,0);
 
 //         dp[0] = true;
 //         for(int i=1; i<=len ;i++)
 //             for(string &w : wordDict){
-//                 int wordSize = w.size();
-//                 if(i>=wordSize && dp[i-wordSize] 
-//                    && s.substr(i-wordSize, wordSize)==w)
+//                 if(i>= w.size() && dp[i- w.size()] 
+//                    && s.substr(i- w.size(),  w.size())==w)
 //                 {
 //                     dp[i] = true;
 //                     break;
@@ -37,22 +53,6 @@ public:
 //             }
 
 //         return dp[len];
-        
-        int len = s.size();
-        vector<bool> dp(len+1,0);
-
-        dp[0] = true;
-        for(int i=1; i<=len ;i++)
-            for(string &w : wordDict){
-                if(i>= w.size() && dp[i- w.size()] 
-                   && s.substr(i- w.size(),  w.size())==w)
-                {
-                    dp[i] = true;
-                    break;
-                }
-            }
-
-        return dp[len];
         
         // ref: https://leetcode.com/problems/word-break/discuss/3179584/C%2B%2B-Easy-Solution-Using-DP
     }
