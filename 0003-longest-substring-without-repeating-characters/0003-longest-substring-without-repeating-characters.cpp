7fb81783652f9e530372f978531bb4226389ecc3
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-24 00:10:36 -0800

    Time: 3 ms (98.64%), Space: 6.9 MB (97.55%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        //int cache[128];
-        vector<int> cache(256, -1);
-        int result = 0, start = -1;
-        int len = s.length();
         
-        for(int i=0; i<len; i++){
-            if(cache[s[i]] > start){
-                start = cache[s[i]];
-            }
-            cache[s[i]] = i;
-            result = max(result, i-start);
+        // vector 90% 63%
+//         vector<int> cache(256, -1);
+//         int len = s.length();        
+//         int result = 0, start = -1;               
+       
+//         for(int i=0; i<len; i++){
+//             if(cache[s[i]] > start){
+//                 start = cache[s[i]];
+//             }
+//             cache[s[i]] = i;
+//             result = max(result, i-start);
+//         }
+//         return result;
+        
+        int store[128]={0}; //array to store the occurences of all the characters
+        int l=0;    
+        int r=0;   
+        int ans=0;  
+        
+        while(r<s.length())    
+        {
+            store[s[r]]++;               
+            while(store[s[r]]>1) {
+                store[s[l]]--;   
+                l++;         
+            }            
+            ans = max(ans,r-l+1);   
+            r++;        
         }
-        return result;
+        return ans;
 
 
         // 50% 20%
