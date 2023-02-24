543e2f1426585897fa148fe7056d4043c290f9f2
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-23 23:59:11 -0800

    Time: 8 ms (89.10%), Space: 8.4 MB (63.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,37 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_map<char, int> myMap;
-        int current=0, maxString=0, start=0;
+        //int cache[128];
+        vector<int> cache(256, -1);
+        int result = 0, start = -1;
         int len = s.length();
         
         for(int i=0; i<len; i++){
-            if(myMap.count(s[i])>0){
-                start = max(start, myMap[s[i]]+1); 
-                myMap[s[i]] = i;  // insert does not override if exist
+            if(cache[s[i]] > start){
+                start = cache[s[i]];
             }
-            myMap.insert({s[i], i}); 
-            current = i-start + 1;
-            maxString = max(maxString, current);                       
+            cache[s[i]] = i;
+            result = max(result, i-start);
         }
+        return result;
+
+
+        // 50% 20%
+//         unordered_map<char, int> myMap;
+//         int current=0, maxString=0, start=0;
+//         int len = s.length();
         
-        return maxString;
+//         for(int i=0; i<len; i++){
+//             if(myMap.count(s[i])>0){
+//                 start = max(start, myMap[s[i]]+1); 
+//                 myMap[s[i]] = i;  // insert does not override if exist
+//             }
+//             myMap.insert({s[i], i}); 
+//             current = i-start + 1;
+//             maxString = max(maxString, current);                       
+//         }
+        
+//         return maxString;
     }
 };
 
