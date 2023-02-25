f4e127fc9a1f7106134bdcdf63cb8372db9ac201
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-24 16:29:11 -0800

    Time: 138 ms (73.54%), Space: 23.9 MB (76.12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,40 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        // O(N^2 + O(NlogN)) = O(N^2)
+        sort(nums.begin(), nums.end()); // O(NlogN)
         
         int len = nums.size();
         int l, r;
         vector<vector<int>> result;
         
-        for(int i=0; i<len-2; i++){
+        for(int i=0; i<len-2; i++){ //O(N^2)
             if(i>0 && nums[i] == nums[i-1]) continue;
             l = i+1;
             r = len-1;
             while(l < r){
-
                 int sum = nums[i] + nums[l] + nums[r];
+                
                 if(sum == 0){
                     result.push_back(vector<int>{nums[i], nums[l], nums[r]});
                     
                     while(l+1<r && nums[l] == nums[l+1]) l++; // l and r has to be different
                     while(r-1>l && nums[r] == nums[r-1]) r--;
+                    // in the while loop, l or r stops at the last repeated number
+                    // => increment 1 more to move on the next number
                     l++; r--;
                 }
-                else if(sum < 0)
+                else if(sum < 0) 
                     l++;
-                else
-                    r--;
-                
+                else 
+                    r--;               
             }
         }
-        //if (result.size() == 0) result = vector<int>{};
         return result;
-        // ignore duplicate number
-        // start from first, second+third = -first using left, right pointers
     }
 };
-
+// because there are high chance to get duplicate triplets, we need to sort first
 // [-1, 0, 1, 2, -1, 4]
 // [-1, -1, 0, 1, 2, 4]
 
