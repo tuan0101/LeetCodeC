15a16a2eafa3c983a5246fb0df674498da0e41b1
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-25 15:29:34 -0800

    Time: 7 ms (67.27%), Space: 8.6 MB (87.21%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.cpp
@@ -0,0 +1,61 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        queue<Node*> q;
+        q.push(node);
+                
+        unordered_map<Node*, Node*> copies;
+        copies[node] = new Node(node->val);
+        
+        Node* current;
+        
+        while(!q.empty()){
+            current = q.front();
+            q.pop();            
+            for(Node* neighbor : current->neighbors){               
+                if(copies.count(neighbor)==0){
+                    copies[neighbor] = new Node(neighbor->val);
+                    q.push(neighbor);                    
+                }
+                copies[current]->neighbors.push_back(copies[neighbor]);
+            }            
+        }
+        return copies[node];
+
+        //return dfs_clone(node);        
+    }
+//private:
+//     unordered_map<Node*, Node*> copies;
+//     Node* dfs_clone(Node* node){
+//         if (!node) return NULL;
+//         if (copies.count(node)==0){
+//             copies[node] = new Node(node->val);
+//             for(Node* neighbor : node->neighbors){
+//                 copies[node]->neighbors.push_back(dfs_clone(neighbor));
+//             }
+//         }
+//         return copies[node];   
+//     }
+};
