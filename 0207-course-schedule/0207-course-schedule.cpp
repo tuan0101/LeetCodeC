724d1b45d2440170a238be2d929061977a1370e6
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-02-28 04:26:28 -0800

    Time: 36 ms (20.39%), Space: 19.2 MB (5.11%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,46 +1,46 @@
 class Solution {
 public:
     // 20% 5%    
-//     unordered_map<int, vector<int>> M;
+       
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> visited(numCourses);
+        vector<vector<int>> M(numCourses);
+        
+        for(vector<int> pre : prerequisites){
+            M[pre[0]].push_back(pre[1]);
+        }
+        
+        // check if each course can be completed
+        for(int course=0; course<numCourses; course++){
+            if (!canComplete(course, visited, M))
+                return false;
+        }
+        return true;
+    }
     
-     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-//         vector<bool> visited(numCourses);
+    // DFS for each pre in a course
+    bool canComplete(int course, vector<bool> visited, vector<vector<int>>& M){
+        if(visited[course]) return false;
+        else visited[course] = true;  // from undefined to true
         
-//         for(vector<int> pre : prerequisites){
-//             M[pre[0]].push_back(pre[1]);
-//         }
-        
-//         // check if each course can be completed
-//         for(int course=0; course<numCourses; course++){
-//             if (!canComplete(course, visited))
-//                 return false;
-//         }
-//         return true;
-//     }
-    
-//     // DFS for each pre in a course
-//     bool canComplete(int course, vector<bool> visited){
-//         if(visited[course]) return false;
-//         else visited[course] = true;  // from undefined to true
-        
-//         if(M[course].size() == 0) return true;
-//         for(int &pre : M[course]){
-//             if (!canComplete(pre, visited))
-//                 return false;               
-//         }
-//         visited[course] = false;
-//         M[course].clear(); // mark as canComplete
-//         return true;
-//     }
-        vector<vector<int>> G(n);
-        vector<int> degree(n, 0), bfs;
-        for (auto& e : prerequisites)
-            G[e[1]].push_back(e[0]), degree[e[0]]++;
-        for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs[i]])
-                if (--degree[j] == 0) bfs.push_back(j);
-        return bfs.size() == n;
-     }
-
+        if(M[course].size() == 0) return true;
+        for(int &pre : M[course]){
+            if (!canComplete(pre, visited, M))
+                return false;               
+        }
+        visited[course] = false;
+        M[course].clear(); // mark as canComplete
+        return true;
+    }
+         
+        //85% 82%
+        // vector<vector<int>> G(n);
+        // vector<int> degree(n, 0), bfs;
+        // for (auto& e : prerequisites)
+        //     G[e[1]].push_back(e[0]), degree[e[0]]++;
+        // for (int i = 0; i < n; ++i) if (!degree[i]) bfs.push_back(i);
+        // for (int i = 0; i < bfs.size(); ++i)
+        //     for (int j: G[bfs[i]])
+        //         if (--degree[j] == 0) bfs.push_back(j);
+        // return bfs.size() == n;
 };
