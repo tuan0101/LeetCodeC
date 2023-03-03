f28146b4b46e4efed51c59e958f30f1348c48602
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-03 00:02:12 -0800

    Time: 36 ms (97.53%), Space: 10 MB (98.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int n) {
-        
-        // 76% 62%
-        // 66% 62% with sort
-        sort(coins.begin(), coins.end());
         int INF = n+1;
-        vector<int> dp (n+1, INF);
+        int dp[n+1];
         
         dp[0] = 0;
-        for(int i=0; i<=n; i++){
+        for(int i=1; i<=n; i++){ 
+            
+            dp[i] = INF;            
             for(int &coin : coins){
                 if(i >= coin)
                     dp[i] = min(dp[i], 1+dp[i-coin]);
-                else
-                    break;
             }
         }
         return dp[n]==INF? -1 : dp[n];
+        
+        // 76% 62%
+        // 77% 70% with sort - kind of same
+//         sort(coins.begin(), coins.end());
+//         int INF = n+1;
+//         vector<int> dp (n+1, INF);
+        
+//         dp[0] = 0;
+//         for(int i=0; i<=n; i++){
+//             for(int &coin : coins){
+//                 if(i >= coin)
+//                     dp[i] = min(dp[i], 1+dp[i-coin]);
+//                 else
+//                     break;
+//             }
+//         }
+//         return dp[n]==INF? -1 : dp[n];
     }
 };
