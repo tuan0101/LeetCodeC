e9ac5373daf7ceb05ae85e9f20b302a2bd65d994
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-04 03:21:46 -0800

    Time: 0 ms (100.00%), Space: 9.6 MB (57.22%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
 
     int mincostTickets(vector<int>& days, vector<int>& costs) {
+        
+        // 70% 57%
         int n = days.size();
         vector<int> dp(n+1, 0);
         dp[n] = 0;
         
         for(int i = n-1; i>=0; i--){
-            dp[i] = min({costs[0] + dp[i+1],  costs[1] + dp[getTheNext8thDay(days, i)], 
-                         costs[2] + dp[getTheNext31stDay(days, i)]});
+            dp[i] = min({costs[0] + dp[i+1],  costs[1] + dp[getTheNext8thDay(days, i, n)], 
+                         costs[2] + dp[getTheNext31stDay(days, i, n)]});
             //dp[i] = min(dp[i], costs[2] + dp[getTheNext31stDay(days, i)] );
             //min({a, b, c}); new syntax
         }
@@ -16,17 +18,17 @@ public:
         return dp[0];
     }
     
-    int getTheNext8thDay(vector<int>& days, int start){
+    int getTheNext8thDay(vector<int>& days, int start, int n){
         int nextDay = days[start] + 7;
-        while(start<days.size() && days[start] < nextDay){
+        while(start<n && days[start] < nextDay){
             start++;
         }
         return start;
     }
     
-    int getTheNext31stDay(vector<int>& days, int start){
+    int getTheNext31stDay(vector<int>& days, int start, int n){
         int nextDay = days[start] + 30;
-        while(start<days.size() && days[start] < nextDay){
+        while(start<n && days[start] < nextDay){
             start++;
         }
         return start;
