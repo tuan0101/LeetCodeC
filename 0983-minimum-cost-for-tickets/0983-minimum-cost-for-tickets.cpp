b23c68dac64b8c3fd27dad448926a6e48c2f94ad
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-04 04:33:52 -0800

    Time: 8 ms (33.15%), Space: 9.7 MB (33.40%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,41 +1,26 @@
 class Solution {
 public:
 
-    int mincostTickets(vector<int>& DAYS, vector<int>& cost) {
-        //
-        unordered_set<int> days(DAYS.begin(),DAYS.end());
-        vector<int> dp(367);
-        
-        for(int i=1 ; i<366 ; i++)
-        {
-            dp[i] = dp[i-1];
-            if(days.find(i) != days.end())
-            {
-			// cost of ith day , decision of ith day will be
-                dp[i] = min({ dp[i - 1] + cost[0],   // 1st case
-                             dp[max(0, i - 7)] + cost[1],  // 2nd case
-                             dp[max(0, i - 30)] + cost[2]});  // 3rd case
-            }
-        }
-        return dp[365];
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
         // 72% 57%
-//         int n = days.size();
-//         vector<int> dp(n+1);
-//         dp[n] = 0;
+         int n = days.size();
+         vector<int> dp(n+1);
+         dp[n] = 0;
         
-//         for(int i = n-1; i>=0; i--){
-//             int theNext8thDay = i;
-//             int theNext31stDay = i;
-//             while(theNext8thDay<n && days[theNext8thDay] < days[i]+7) theNext8thDay++;
-//             while(theNext31stDay<n && days[theNext31stDay] < days[i]+30) theNext31stDay++;
+         for(int i = n-1; i>=0; i--){
+             int theNext8thDay = i;
+             int theNext31stDay = i;
+            // find the index on the 8th day 
+            // start couting from the day purchase week ticket
+             while(theNext8thDay<n && days[theNext8thDay] < days[i]+7) theNext8thDay++;
+            // find the index on the 31st day 
+            // start counting from the day purchase month ticket
+             while(theNext31stDay<n && days[theNext31stDay] < days[i]+30) theNext31stDay++;
             
-//             dp[i] = min({costs[0] + dp[i+1],  costs[1] + dp[theNext8thDay], 
-//                          costs[2] + dp[theNext31stDay]});
-//         }        
-//         return dp[0];
+             dp[i] = min({costs[0] + dp[i+1],  costs[1] + dp[theNext8thDay], 
+                          costs[2] + dp[theNext31stDay]});
+         }        
+         return dp[0];
     }
 };
-
-// 1 4 6 7 8 20
-// 7...... 2 2
-// 1 7
