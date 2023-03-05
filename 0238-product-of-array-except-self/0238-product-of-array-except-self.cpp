6f87b325f90c4df028905b43a6581aca99b393ad
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-05 04:36:04 -0800

    Time: 23 ms (85.24%), Space: 24.1 MB (55.28%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,21 +1,36 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int prefix = 1;
-        vector<int> answer;
+        int prefix = 1, postfix = 1;
+        int n = nums.size();
+        vector<int> answer (n,1);
         
-        for(int &num : nums){
-            answer.push_back(prefix);
-            prefix *= num;
+        for(int i=0; i<n; i++){
+            answer[i] *= prefix;
+            prefix *= nums[i];
+            answer[n-i-1] *= postfix;
+            postfix *= nums[n-i-1];
         }
-        
-        int postfix = 1;
-        for(int i=nums.size()-1; i>=0; i--){
-            answer[i] *= postfix;
-            postfix *= nums[i];
-        }
-
+        // i = 3    end = 0
+        // prefix = 6
+        // postfix = 24
+        // [24, 12, 8, 6]
         return answer;
+//         int prefix = 1;
+//         vector<int> answer;
+        
+//         for(int &num : nums){
+//             answer.push_back(prefix);
+//             prefix *= num;
+//         }
+        
+//         int postfix = 1;
+//         for(int i=nums.size()-1; i>=0; i--){
+//             answer[i] *= postfix;
+//             postfix *= nums[i];
+//         }
+
+//         return answer;
     }
 };
 
