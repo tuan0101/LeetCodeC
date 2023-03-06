d2eb11c6f180090e9ca8ef771f56fdce04d99b0b
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-06 06:43:31 -0800

    Time: 3 ms (96.56%), Space: 13.1 MB (85.18%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -19,7 +19,7 @@ public:
         
         while(!q.empty()){
             int q_size = q.size();
-            int count = 0;
+            //int count = 0;
 
             for(int i=0; i<q_size; i++){
                 pair<int,int> p=q.front();
@@ -27,13 +27,13 @@ public:
                 int c = p.second;
                 q.pop();
 
-                bfs(grid, r-1, c, count); // top  
-                bfs(grid, r, c-1, count); // left
-                bfs(grid, r+1, c, count); // bottom
-                bfs(grid, r, c+1, count); // right
+                bfs(grid, r-1, c); // top  
+                bfs(grid, r, c-1); // left
+                bfs(grid, r+1, c); // bottom
+                bfs(grid, r, c+1); // right
             }
 
-            //if(count>0)  IDK why this one cause significantly slower
+            //if(count>0)  
             minute++;                         
         }
         if (fresh>0) return -1;
@@ -42,13 +42,13 @@ public:
         
     }
     
-    void bfs(vector<vector<int>>& grid, int r, int c, int &count){
+    void bfs(vector<vector<int>>& grid, int r, int c){
          if(r<0 || c<0 || r> grid.size()-1 || c>grid[0].size()-1 || grid[r][c]!=1)
              return;
                   
         grid[r][c]=2;
         fresh--;
-        count++;
+        //count++;
         q.push({r, c});    
     }
 };
