9eb55af93ebfc1c627d473b9082c3c4abb8d6505
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-07 09:30:52 -0800

    Time: 12 ms (42.44%), Space: 10.8 MB (60.27%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+private:
+    vector<int> combination;
+    vector<vector<int>> result;
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        validateSum(candidates, target, 0);
+        return result;
+    }
+    
+    void validateSum(vector<int>& candidates, int target, int i){
+        if(target == 0){
+            result.push_back(combination);
+            return;
+        }
+        // check i is valid before sum
+        if(i >= candidates.size() || target < 0)
+            return;
+               
+        // 80%, 60%
+        // combination.push_back(candidates[i]);
+        // validateSum(candidates, target-candidates[i], i);   
+        // combination.pop_back();
+        // validateSum(candidates, target , i + 1); 
+        
+        // starting from i
+        for(int j=i; j<candidates.size(); j++){
+            combination.push_back(candidates[j]);
+            validateSum(candidates, target-candidates[j], j);   
+            combination.pop_back();
+        }
+    }
+};
