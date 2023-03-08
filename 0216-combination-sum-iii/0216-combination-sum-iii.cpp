be70de383098fee34081764b1dfcbaf7f74a86ba
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-08 02:23:23 -0800

    Time: 0 ms (100.00%), Space: 6.6 MB (45.90%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum3(int k, int n) {
+        int candidates[] = {1,2,3,4,5,6,7,8,9};
+        vector<vector<int>> result;
+        vector<int> comb;
+        sum(result, comb, k, n, 1);
+        return result;
+    }
+    
+    void sum(vector<vector<int>> &result, vector<int> &comb, int k, int target, int candidate){
+        if(target == 0 && k==0){
+            result.push_back(comb);
+            return;
+        }
+        
+        if(candidate>9 || k<=0 ||  candidate > target)
+            return;
+        
+        // include candidate in combination
+        comb.push_back(candidate);
+        sum(result, comb, k-1, target-candidate, candidate+1);
+
+        // not include candidate in combination
+        comb.pop_back();
+        sum(result, comb, k, target, candidate+1);
+    }
+};
