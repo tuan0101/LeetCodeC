5cb70ae3f2c6cd216d2a68744b46ed8010f82da9
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-08 01:57:48 -0800

    Time: 8 ms (47.10%), Space: 10.6 MB (87.77%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -3,29 +3,42 @@ private:
     vector<vector<int>> result;
     vector<int> comb;
 public:
+    
+    // 62% 40%
+    // O(2^N)
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end()); //O(NlogN)
         sum(candidates, target, 0);
         return result;
     }
     
-    void sum(vector<int>& candidates, int target, int i){
+    void sum(vector<int>& candidates, int target, int begin){
         if(target == 0){
             result.push_back(comb);
             return;
         }
         
-        if(i>= candidates.size() || candidates[i] > target)
+        if(begin>= candidates.size() || candidates[begin] > target)
             return;
         
-        comb.push_back(candidates[i]);
-        sum(candidates, target - candidates[i], i+1);
-        comb.pop_back();
-        // ignore all repeated current number
-        while(i<candidates.size()-1 && candidates[i+1]==candidates[i]){
-            i++;
-        }        
-        sum(candidates, target, i+1);
+        // comb.push_back(candidates[i]);
+        // sum(candidates, target - candidates[i], i+1);
+        // comb.pop_back();
+        // // ignore all repeated current number
+        // while(i<candidates.size()-1 && candidates[i+1]==candidates[i]){
+        //     i++;
+        // }        
+        // sum(candidates, target, i+1);
+        int prev = -1;
+        for(int i=begin; i<candidates.size(); i++){
+            if(candidates[i]==prev) continue;
+            
+            comb.push_back(candidates[i]);
+            sum(candidates, target - candidates[i], i+1);
+            comb.pop_back();
+            prev = candidates[i];
+        }
+            
     }
 };
 
