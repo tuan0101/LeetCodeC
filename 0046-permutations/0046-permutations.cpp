afb770a6f5bc02d464905af570afa3ae7c5ed53b
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-09 06:01:16 -0800

    Time: 0 ms (100.00%), Space: 7.8 MB (46.03%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,45 +1,47 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        // vector<vector<int>> result;
-        // vector<int> comb;
-        // bool used[nums.size()]; 
-        // memset(used, false, sizeof nums);
-        // getPermutation(result, comb, nums, used);
-        // return result;
-        vector<vector<int>> perms;
-        permute(nums, 0, perms);
-        return perms;
+        vector<vector<int>> result;
+        vector<int> comb;
+        const int n = nums.size();
+        vector<bool> used(nums.size(), false);
+        getPermutation(result, comb, nums, used);
+        return result;
+        // vector<vector<int>> perms;
+        // permute(nums, 0, perms);
+        // return perms;
     }
     
-//     void getPermutation(vector<vector<int>>& result, vector<int>& comb, vector<int>& nums, bool used[]){
-//         if(comb.size()==nums.size()){
-//             result.push_back(comb);
-//             return;
-//         }
-        
-//         for(int i=0; i<nums.size(); i++){
-//             if(used[i]) continue;
-//             comb.push_back(nums[i]);
-//             used[i] = true;
-//             getPermutation(result, comb, nums, used);
-//             used[i] = false;
-//             comb.pop_back();
-//         }       
-//         delete[] used;
-//     }
-    
-        // swap 100% 20%
-        void permute(vector<int> nums, int i, vector<vector<int>>& perms) {
-        if (i == nums.size()) {
-            perms.push_back(nums);
-        } else {
-            for (int j = i; j < nums.size(); j++) {
-                swap(nums[i], nums[j]);
-                permute(nums, i + 1, perms);
-            }
+    void getPermutation(vector<vector<int>>& result, vector<int>& comb, vector<int>& nums, vector<bool>& used){
+        if(comb.size()==nums.size()){
+            result.push_back(comb);
+            return;
         }
+        
+        for(int i=0; i<nums.size(); i++){
+            if(used[i]) continue;
+            // make choice
+            comb.push_back(nums[i]);
+            used[i] = true;
+            // this line finishes when it cannot make choice anymore
+            // or reach goal
+            getPermutation(result, comb, nums, used);
+            // undo the choice
+            used[i] = false;
+            comb.pop_back();
+        }       
     }
+    
+        // swap 10-100% 20%
+        // void permute(vector<int> nums, int i, vector<vector<int>>& perms) {
+        // if (i == nums.size()) {
+        //     perms.push_back(nums);
+        // } else {
+        //     for (int j = i; j < nums.size(); j++) {
+        //         swap(nums[i], nums[j]);
+        //         permute(nums, i + 1, perms);
+        //     }
+        // }
+    //}
 };
            
-// comb: [1, 3]
