8e7ce16ec91c22fc403c7d0ed160a19c2a8baf2c
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-13 07:44:42 -0700

    Time: 27 ms (57.06%), Space: 13.7 MB (93.81%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,59 +1,79 @@
-// 73% 77%
-class DisjointSet{
-private:
-    vector<int> root;
-    vector<int> rank;
-    int count;
-public:
-    DisjointSet(int sz):root(sz), rank(sz), count(sz){
-        for(int i=0; i<sz; i++){
-            root[i] = i;
-            rank[i] = i;
-        }        
-    }
+// 30-73% 60-77%
+// class DisjointSet{
+// private:
+//     vector<int> root;
+//     vector<int> rank;
+//     int count;
+// public:
+//     DisjointSet(int sz):root(sz), rank(sz), count(sz){
+//         for(int i=0; i<sz; i++){
+//             root[i] = i;
+//             rank[i] = i;
+//         }        
+//     }
     
-    int find(int x){
-        if(root[x] == x)
-            return x;
-        return root[x] = find(root[x]);
-    }
+//     int find(int x){
+//         if(root[x] == x)
+//             return x;
+//         return root[x] = find(root[x]);
+//     }
     
-    void unionSet(int x, int y){
-        int xroot = find(x);
-        int yroot = find(y);
+//     void unionSet(int x, int y){
+//         int xroot = find(x);
+//         int yroot = find(y);
         
-        if(xroot == yroot) return;
+//         if(xroot == yroot) return;
         
-        if(rank[xroot] > rank[yroot]){
-            root[yroot] = xroot;
-        }else if(rank[xroot] < rank[yroot]){
-            root[xroot] = yroot;
-        }else{
-            root[yroot] = xroot;
-            rank[xroot]++;
-        }
-        count--;
-    }
+//         if(rank[xroot] > rank[yroot]){
+//             root[yroot] = xroot;
+//         }else if(rank[xroot] < rank[yroot]){
+//             root[xroot] = yroot;
+//         }else{
+//             root[yroot] = xroot;
+//             rank[xroot]++;
+//         }
+//         count--;
+//     }
     
-    int getCount(){
-        return count;
-    }
-};
+//     int getCount(){
+//         return count;
+//     }
+// };
 
 class Solution {
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        int n = isConnected.size();
-        DisjointSet ds(n);
+//         int n = isConnected.size();
+//         DisjointSet ds(n);
         
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(isConnected[i][j]==1)
-                    ds.unionSet(i, j);
+//         for(int i=0; i<n; i++){
+//             for(int j=0; j<n; j++){
+//                 if(isConnected[i][j]==1)
+//                     ds.unionSet(i, j);
+//             }
+//         }
+        
+//         return ds.getCount();
+        int n = isConnected.size();
+        int result=0;
+        vector<bool> visited(n, false);
+        for(int i=0; i<n; i++){           
+            if(!visited[i]){
+                dfs(isConnected, visited, i);
+                result++;
             }
         }
+        return result;
+    }
+    
+    void dfs(vector<vector<int>>& M, vector<bool>& visited, int i){
+        visited[i] = true;
         
-        return ds.getCount();
+        for(int j=0; j<M.size(); j++){
+            if(M[i][j]==1 && !visited[j] && i!=j)
+                dfs(M, visited, j);
+        }
+        
     }
 };
 
