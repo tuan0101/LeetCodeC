2e0d633e6ed2db45cda8d7ef2de185c802a7d400
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-03-17 06:42:26 -0700

    Time: 103 ms (92.49%), Space: 95.9 MB (20.05%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:      
+    vector<vector<int>> dp;
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(begin(nums), end(nums), 0), target = sum / 2;
-        if(sum & 1) return false;
-        bool dp[target+1]; memset(dp, false, sizeof dp);
-        dp[0] = true;                              // 0 sum is always achievable
-        for(int num : nums) 
-            for(int j = target; j >= num; j--)    // essential to start right to left
-                if(dp[j - num])                    // if j - num was previously achievable
-                    dp[j] = true;                  // we can add num to it and make j achievable as well
+        // int sum = accumulate(begin(nums), end(nums), 0), target = sum / 2;
+        // if(sum & 1) return false;
+//         bool dp[target+1]; memset(dp, false, sizeof dp);
+//         dp[0] = true;                              // 0 sum is always achievable
+//         for(int num : nums) 
+//             for(int j = target; j >= num; j--)    // essential to start right to left
+//                 if(dp[j - num])                    // if j - num was previously achievable
+//                     dp[j] = true;                  // we can add num to it and make j achievable as well
             
-        return dp[target];      
+//         return dp[target];      
+        int totalSum = accumulate(begin(nums), end(nums), 0);
+        if(totalSum & 1) return false;
+        int sum = totalSum/2;
+        dp.clear();
+        dp.resize(nums.size()+1, vector<int>(sum +1, -1));
+        return subsetSum(nums, sum);
     }
+    bool subsetSum(vector<int>& nums, int sum, int i = 0) {
+        if(sum == 0) return true;
+        if(i >= size(nums) || sum < 0) return false; 
+        if(dp[i][sum] != -1) return dp[i][sum];
+        return dp[i][sum] = subsetSum(nums, sum - nums[i], i + 1) || subsetSum(nums, sum, i + 1);
+    }
+    
 };
 // [2,2,3,5]
 // dp[2-2] dp[2] = true
