b953828c79e19ece5dfb98f1f56c3a458c869700
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-04-02 06:53:26 -0700

    Time: 3 ms (99.26%), Space: 6.9 MB (76.24%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -4,14 +4,27 @@ private:
     int length = 0;
 public:
     string longestPalindrome(string s) {
-        for(int i=0; i<s.size(); i++){
-            checkPalindrome(s, i, i);
-            checkPalindrome(s, i, i+1);
-        }
-        return result;
+        // for(int i=0; i<s.size(); i++){
+        //     checkPalindrome(s, i, i);
+        //     checkPalindrome(s, i, i+1);
+        // }
+        // return result;
+            if (s.empty()) return "";
+    if (s.size() == 1) return s;
+    int min_start = 0, max_len = 1;
+    for (int i = 0; i < s.size();) {
+      if (s.size() - i <= max_len / 2) break;
+      int j = i, k = i;
+      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
+      i = k+1;
+      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
+      int new_len = k - j + 1;
+      if (new_len > max_len) { min_start = j; max_len = new_len; }
+    }
+    return s.substr(min_start, max_len);
     }
     
-    // 10% 5%
+    // 56% 17%
     void checkPalindrome(string s, int l, int r){
         while(l>=0 && r<s.size() && s[l] == s[r]){
             if(r-l+1 > length){
