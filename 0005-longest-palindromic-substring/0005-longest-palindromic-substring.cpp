86f20df7375bef71a6a2cf9e8a6d2fd0f566b027
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-04-02 07:31:35 -0700

    Time: 4 ms (98.21%), Space: 7.5 MB (64.28%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,56 +1,35 @@
 class Solution {
-private:
-    string result;
-    int length = 0;
 public:
+    
     string longestPalindrome(string s) {
-        // for(int i=0; i<s.size(); i++){
-        //     checkPalindrome(s, i, i);
-        //     checkPalindrome(s, i, i+1);
-        // }
-        // return result;
-        //if(s.size() < 2) return s;
         int max_len = 0;
-        int start_idx = 0;
-        int i = 0;
-        while(i < s.size()) {
-            int r_ptr = i; 
-            int l_ptr = i;
-            //find the middle of a palindrome
-            while(r_ptr < s.size()-1 && s[r_ptr] == s[r_ptr + 1]) r_ptr++;
-            i = r_ptr+1;
-            //expand from the middle out
-            while(r_ptr < s.size()-1 && l_ptr > 0 && s[r_ptr + 1] == s[l_ptr - 1]) {
-                r_ptr++;
-                l_ptr--;
+        int i=0;
+        int start = 0;
+        string res;
+        while(i < s.size()){
+            int l = i, r = i;
+            
+            // by skipping duplicate char, no need to check odd or even palindrom
+            while(r<s.size()-1 && s[r] == s[r+1]) r++;
+            i = r+1;
+            while(l>0 && r<s.size()-1 && s[r+1] == s[l-1]){
+                l--; r++;
             }
-            int new_len = r_ptr - l_ptr + 1;
-            if(new_len > max_len) {
-                start_idx = l_ptr;
+            
+            int new_len = r-l+1;
+            if(new_len > max_len){
                 max_len = new_len;
+                //start = l;
+                res = s.substr(l, max_len);
             }
         }
-        return s.substr(start_idx, max_len);
-    }
-    
-    // 56% 17%
-    void checkPalindrome(string s, int l, int r){
-        while(l>=0 && r<s.size() && s[l] == s[r]){
-            if(r-l+1 > length){
-                result = s.substr(l, r-l+1);
-                length = r-l+1 ;
-            }
-            l--;
-            r++;                
-        }
-    }
-    
-    
+        return res; //s.substr(start, max_len);
+    }  
 };
 
 
 
-// s = "babad"
+// s = "abbbbbad"
 // 01
 // babad
 // dp[1] = 1
