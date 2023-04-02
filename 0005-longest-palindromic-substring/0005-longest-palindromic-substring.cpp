1cba8ce5fc6c4a731b225cfedd5663b976c8717d
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-04-02 06:55:02 -0700

    Time: 0 ms (100.00%), Space: 6.9 MB (76.24%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -9,19 +9,28 @@ public:
         //     checkPalindrome(s, i, i+1);
         // }
         // return result;
-            if (s.empty()) return "";
-    if (s.size() == 1) return s;
-    int min_start = 0, max_len = 1;
-    for (int i = 0; i < s.size();) {
-      if (s.size() - i <= max_len / 2) break;
-      int j = i, k = i;
-      while (k < s.size()-1 && s[k+1] == s[k]) ++k; // Skip duplicate characters.
-      i = k+1;
-      while (k < s.size()-1 && j > 0 && s[k + 1] == s[j - 1]) { ++k; --j; } // Expand.
-      int new_len = k - j + 1;
-      if (new_len > max_len) { min_start = j; max_len = new_len; }
-    }
-    return s.substr(min_start, max_len);
+        if(s.size() < 2) return s;
+        int max_len = 0;
+        int start_idx = 0;
+        int i = 0;
+        while(i < s.size()) {
+            int r_ptr = i; 
+            int l_ptr = i;
+            //find the middle of a palindrome
+            while(r_ptr < s.size()-1 && s[r_ptr] == s[r_ptr + 1]) r_ptr++;
+            i = r_ptr+1;
+            //expand from the middle out
+            while(r_ptr < s.size()-1 && l_ptr > 0 && s[r_ptr + 1] == s[l_ptr - 1]) {
+                r_ptr++;
+                l_ptr--;
+            }
+            int new_len = r_ptr - l_ptr + 1;
+            if(new_len > max_len) {
+                start_idx = l_ptr;
+                max_len = new_len;
+            }
+        }
+        return s.substr(start_idx, max_len);
     }
     
     // 56% 17%
