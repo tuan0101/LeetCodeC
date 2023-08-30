c6ccf2b3f6b64ae340823b1fe2a24786128de674
Tuan <60296627+tuan0101@users.noreply.github.com>
2023-08-30 13:42:38 -0700

    Time: 8 ms (85.05%), Space: 6.9 MB (93.68%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         // vector 90% 63%
         //vector<int> cache(256, -1);
         
-        // array 93%  97%
+        //array 93%  97%
         //int cache[128];     // there is no default value
         int cache[128]={};  // initialize all to 0
         int len = s.length();        
@@ -29,21 +29,21 @@ public:
         // when i=7, start = 4, result = 4
 
         // 50% 20%
-//         unordered_map<char, int> myMap;
-//         int current=0, maxString=0, start=0;
-//         int len = s.length();
+        // unordered_map<char, int> myMap;
+        // int current=0, maxString=0, start=0;
+        // int len = s.length();
         
-//         for(int i=0; i<len; i++){
-//             if(myMap.count(s[i])>0){
-//                 start = max(start, myMap[s[i]]+1); 
-//                 myMap[s[i]] = i;  // insert does not override if exist
-//             }
-//             myMap.insert({s[i], i}); 
-//             current = i-start + 1;
-//             maxString = max(maxString, current);                       
-//         }
+        // for(int i=0; i<len; i++){
+        //     if(myMap.count(s[i])>0){
+        //         start = max(start, myMap[s[i]]+1); 
+        //         myMap[s[i]] = i;  // insert does not override if exist
+        //     }
+        //     myMap.insert({s[i], i}); 
+        //     current = i-start + 1;
+        //     maxString = max(maxString, current);                       
+        // }
         
-//         return maxString;
+        // return maxString;
     }
 };
 
